0995a13e6177ddc747e9fb9c147941b888a3b5d0
周高锋 <tzjz6065278@163.com>
2023-08-15 14:27:25 +0800

    feat: init

--- play/app.vue
+++ play/app.vue
@@ -1,5 +1,6 @@
 <template>
   <eLong-button plain type="primary">朴素按钮</eLong-button>
+
   <ELongButton round type="primary">椭圆</ELongButton>
   <ELongButton disabled type="primary">禁止</ELongButton>
   <ELongButton circle type="primary">圆</ELongButton>
